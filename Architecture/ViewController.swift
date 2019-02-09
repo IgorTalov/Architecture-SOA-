3c13a63dbbf8bf62bf6648fba2e63bba6021a4a0
Игорь Талов <igortalov@gmail.com>
2019-02-09 20:56:50 +0300

    Initial Commit

---
+++ Architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Architecture
+//
+//  Created by Игорь Талов on 09/02/2019.
+//  Copyright © 2019 IgorTalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
